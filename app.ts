56994f89e12b8d6a949be5c9665e5227409ac852
Gabriel Evangelista <gabriel.evan.queiroz@gmail.com>
2017-09-11 22:46:44 -0300

    Variables and functions

---
+++ app.ts
@@ -0,0 +1,31 @@
+let messege: string = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(messege);
+
+
+let episode: number = 4
+console.log("This is episode " + 4);
+episode = episode + 1
+console.log("Next episode is " + episode);
+
+let favorateDroid: string
+favorateDroid = 'BB-8'
+console.log("My favorite droid is " + favorateDroid)
+
+let isEnoughToBeatMF = function (parsecs: number) : boolean {
+    return parsecs < 12
+}
+
+let distance = 11 
+console.log(`Is ${distance} parsecs enough to beat Millennium Falcon? ${isEnoughToBeatMF(distance) ? 'Yes' : 'No'}`)
+//arrou func
+let call = (name: string) => console.log(`Do you copy, ${name}?`)
+call('R2')
+
+
+//parametros padroes
+function inc(speed: number, inc: number = 1) : number {
+    return speed + inc
+}
+
+console.log(`inc (5,1) = ${inc(5,1)}`)
+console.log(`inc (5) = ${inc(5)}`)
---
+++ dist/app.js
@@ -0,0 +1,24 @@
+var messege = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(messege);
+var episode = 4;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+var favorateDroid;
+favorateDroid = 'BB-8';
+console.log("My favorite droid is " + favorateDroid);
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 11;
+console.log("Is " + distance + " parsecs enough to beat Millennium Falcon? " + (isEnoughToBeatMF(distance) ? 'Yes' : 'No'));
+//arrou func
+var call = function (name) { return console.log("Do you copy, " + name + "?"); };
+call('R2');
+//parametros padroes
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5,1) = " + inc(5, 1));
+console.log("inc (5) = " + inc(5));
